5c0986279874806299cd362e2d100c3f728d9f52
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-09-02 11:42:19 +0900

    remove node module

--- node_modules/@inquirer/prompts/node_modules/@inquirer/core/dist/cjs/types/lib/Paginator.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The paginator keeps track of a pointer index in a list and returns
- * a subset of the choices if the list is too long.
- */
-export declare class Paginator {
-    pointer: number;
-    lastIndex: number;
-    paginate(output: string, active: number, pageSize?: number): string;
-}
