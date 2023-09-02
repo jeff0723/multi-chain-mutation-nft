c6b094cd74bbca41b33ecf550ef3d48f62665c8b
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-09-02 23:04:36 +0900

    add ccip

--- packages/hardhat/contracts/HatchlingCCIP.sol
+++ packages/hardhat/contracts/HatchlingCCIP.sol
@@ -1,45 +1,60 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
-import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IAny2EVMMessageReceiver} from "../interfaces/IAny2EVMMessageReceiver.sol";
 
-contract HatchlingCCIP is ERC721, ERC721URIStorage, Ownable, CCIPReceiver {
-    uint256 public tokenIdCounter;
+import {Client} from "../libraries/Client.sol";
 
-    address _to;
-    string _tokenUri;
+import {IERC165} from "../../vendor/openzeppelin-solidity/v4.8.0/utils/introspection/IERC165.sol";
 
-    constructor(
-        address router
-    ) CCIPReceiver(router) ERC721("Hatchling", "HL") {}
+/// @title CCIPReceiver - Base contract for CCIP applications that can receive messages.
+abstract contract CCIPReceiver is IAny2EVMMessageReceiver, IERC165 {
+    address internal immutable i_router;
 
-    function _ccipReceive(
-        Client.Any2EVMMessage memory message
-    ) internal override {
-        (_to, _tokenUri) = abi.decode(message.data, (address, string));
-        _safeMint(_to, tokenIdCounter);
-        _setTokenURI(tokenIdCounter, _tokenUri);
-        tokenIdCounter++;
+    constructor(address router) {
+        if (router == address(0)) revert InvalidRouter(address(0));
+        i_router = router;
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
+    /// @notice IERC165 supports an interfaceId
+    /// @param interfaceId The interfaceId to check
+    /// @return true if the interfaceId is supported
     function supportsInterface(
         bytes4 interfaceId
-    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    ) public pure override returns (bool) {
+        return
+            interfaceId == type(IAny2EVMMessageReceiver).interfaceId ||
+            interfaceId == type(IERC165).interfaceId;
     }
 
-    function _burn(
-        uint256 tokenId
-    ) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+    /// @inheritdoc IAny2EVMMessageReceiver
+    function ccipReceive(
+        Client.Any2EVMMessage calldata message
+    ) external virtual override onlyRouter {
+        _ccipReceive(message);
+    }
+
+    /// @notice Override this function in your implementation.
+    /// @param message Any2EVMMessage
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal virtual;
+
+    /////////////////////////////////////////////////////////////////////
+    // Plumbing
+    /////////////////////////////////////////////////////////////////////
+
+    /// @notice Return the current router
+    /// @return i_router address
+    function getRouter() public view returns (address) {
+        return address(i_router);
+    }
+
+    error InvalidRouter(address router);
+
+    /// @dev only calls from the set router are accepted.
+    modifier onlyRouter() {
+        if (msg.sender != address(i_router)) revert InvalidRouter(msg.sender);
+        _;
     }
 }
